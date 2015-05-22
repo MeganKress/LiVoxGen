22aeb99baf0b31812ce7f94a1a8af36a22a56e8e
Megan Kress <MeganKress@users.noreply.github.com>
2015-05-22 11:51:02 -0400

    Update email.php

--- email.php
+++ email.php
@@ -1,10 +1,13 @@
 <html>
 <body>
 
-<?php
-    mail('kressmeg@msu.edu', "LiVoxGen Email", $_POST['comment']);
-?>
-<p>Your email has been sent.</p>
+#<?php
+#    mail('kressmeg@msu.edu', "LiVoxGen Email", $_POST['comment']);
+#?>
+#<p>Your email has been sent.</p>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["mail"]; ?>
 
 </body>
 </html>
